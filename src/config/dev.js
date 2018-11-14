a42bfd129abb33d7126814268bfbac4e9898de4d
wellfrog <wellfrog16@gmail.com>
2018-11-14 14:34:18 +0800

    临时增加菜单项目

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,5 +1,5 @@
 const config = {
-    apiServer: 'http://localhost:8001',
+    apiServer: 'http://api.dreamersky.com',
     uploadUrl: 'http://localhost:8001/service/file/upload',
     imgServer: 'http://127.0.0.1:50625'
 };
